c5b2222c61d4c337180ffe9ef082b948ed200f89
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2018-05-16 16:29:28 -0700

    commit the updated game, playing with the computer

--- Rock_Paper_Scissors/basic.py
+++ Rock_Paper_Scissors/basic.py
@@ -3,21 +3,47 @@ print("...Paper...")
 print("...Scissors...")
 
 player1 = input("Player1, make your move : ")
+print("**** NO CHEATING***\n" * 30)
 player2 = input("Player2, make your move : ")
 
-if player1 == "rock" and player2 == "scissors":
-	print("player1 wins!!")
-elif player1 == "rock" and player2 == "paper":
-	print("player2 wins!!")
-elif player1 == "paper" and player2 == "rock":
-	print("player1 wins!!")
-elif player1 == "paper" and player2 == "scissors":
-	print("player2 wins!!")
-elif player1 == "scissors" and player2 == "rock":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "paper":
-	print("player1 wins!!")
-elif player1 == player2:
-	print("Its a tie")
+if player1 == player2:
+	print("its a tie")
+elif player1 == "rock":
+	if player2 == "scissors":
+		print("player1 wins!!")
+	elif player2 == "paper":
+		print("player2 wins!")
+elif player1 == "paper":
+	if player2 == "scissors":
+		print("player2 wins!!")
+	elif player2 == "rock":
+		print("player1 wins!")
+elif player1 == "scissors":
+	if player2 == "rock":
+		print("player2 wins!!")
+	elif player2 == "paper":
+		print("player1 wins!")
 else:
 	print("Something went wrong :( ")
+
+
+
+
+
+
+# if player1 == "rock" and player2 == "scissors":
+# 	print("player1 wins!!")
+# elif player1 == "rock" and player2 == "paper":
+# 	print("player2 wins!!")
+# elif player1 == "paper" and player2 == "rock":
+# 	print("player1 wins!!")
+# elif player1 == "paper" and player2 == "scissors":
+# 	print("player2 wins!!")
+# elif player1 == "scissors" and player2 == "rock":
+# 	print("player2 wins!")
+# elif player1 == "scissors" and player2 == "paper":
+# 	print("player1 wins!!")
+# elif player1 == player2:
+# 	print("Its a tie")
+# else:
+# 	print("Something went wrong :( ")
