f5ac7fd9b84072fe37a6c7a64a52890c709978f6
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2018-05-17 13:26:40 -0700

    last looping assignment commit

---
+++ Loops/break.py
@@ -0,0 +1,9 @@
+# while True:
+# 	word = input("Type 'exit' to exit :")
+# 	if (word  == "exit"):
+# 		break
+
+for x in range(1 , 200):
+	print(x)
+	if (x == 5):
+		break
---
+++ Loops/clean_room.py
@@ -0,0 +1,7 @@
+times = int(input("How many times do I have to tell you??"))
+
+for time in range(times):
+	print("CLEAN UP YOUR ROOM!!")
+	if time >= 3:
+		print("Why don't you listen to me!!!!!")
+		break
